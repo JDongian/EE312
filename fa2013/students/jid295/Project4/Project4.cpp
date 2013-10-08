b6f3e90543393af508d86fb564de007d1d75aaf1
Joshua Dong <jdong42@gmail.com>
2013-10-08 00:04:01 -0500

    save

--- fa2013/students/jid295/Project4/Project4.cpp
+++ fa2013/students/jid295/Project4/Project4.cpp
@@ -1,9 +1,9 @@
 /*
- * Project9.c
+ * Project4.c
  *
- * My Name
- * My TA
- * My Section Time
+ * Joshua Dong
+ * Chris
+ * F
  *
  *
  */
@@ -11,6 +11,10 @@
 
 #include <stdio.h>
 #include "MazeParams.h"
+#define MIN_DOUBLE 1e-15
+#define VALUE_PENNY 1
+#define VALUE_NICKLE 5
+#define VALUE_DODEK 12
 
 /* return the smallest of the elements in array x[]
  * there are n elements in x[] (x[0].. x[n-1])
@@ -85,7 +89,6 @@ int minRec2(int x[], int n) {
  * accuracy.
  */
 double sqrtIt(double x, double low_guess, double high_guess) {
-    //double min_inc = 0.00000000000001;
     return 0;
 }
 
@@ -102,29 +105,30 @@ double sqrtIt(double x, double low_guess, double high_guess) {
  * accuracy.
  */
 double sqrtRec(double x, double low_guess, double high_guess) {
-    double MIN_DOUBLE = 0.000000000000001;
     /* Please no negative please please */
-    if (low_guess <= 0) {
-        low_guess = MIN_DOUBLE;
+    if (high_guess <= 0 or x <= 0) {
+        printf("Out of domain.\n");
+        return 0;
     }
     double mid_guess = (low_guess+high_guess)/2;
     double mid_result = x/mid_guess;
     /* If accuracy is good enough, return. */
-    if (mid_guess <= mid_result &&
-        mid_guess >= mid_result-MIN_DOUBLE) {
+    if (((high_guess - low_guess)/low_guess) < MIN_DOUBLE) {
         return mid_guess;
+    }
     /* If mid_guess lowers the max bound and
      * mid raises the min bound.
      */
-    } else if (mid_result < mid_guess) {
-        printf("%f, %f:%f, %f:%f\n", x, low_guess, minimum, high_guess, maximum);
-        return sqrtRec(x, mid_result, mid_guess);
+    if (mid_result < mid_guess) {
+        //printf("low=%f, %f (%f), high=(%f)\n", low_guess, mid_guess, mid_result, high_guess);
+        return sqrtRec(x, low_guess, mid_guess); 
+    }
     /* If mid_guess raises the min bound and
      * mid lowers the max bound.
      */
-    } else if (mid_result > mid_guess) {
-        printf("%f, %f:%f, %f:%f\n", x, low_guess, minimum, high_guess, maximum);
-        return sqrtRec(x, mid_guess, mid_result);
+    if (mid_result > mid_guess) {
+        //printf("high=%f, (%f) %f, low=(%f)\n", high_guess, mid_guess, mid_result, low_guess);
+        return sqrtRec(x, mid_guess, high_guess);
     }
     printf("Error.\n");
 }
@@ -145,10 +149,24 @@ double sqrtRec(double x, double low_guess, double high_guess) {
  */
 
 int strCompare(char* str1, char* str2) {
-    if (*str1 * *str2 == 0) {
-        *str1 == *str2 ? return true : return false;
+    if (*str1 && *str2) {
+        /* If str1 is less than str2 */
+        if (*str1 < *str2) {
+            return -1;
+        }
+        /* If str1 is greater than str2 */
+        if (*str1 > *str2) {
+            return 1;
+        }
+        return strCompare(str1+1, str2+1);
     }
-    *str1 == *str2 ? return strCompare(str1+1, str2+1): return false;
+    if (!*str1) {
+        return -1;
+    }
+    if (!*str2) {
+        return 1;
+    }
+    return 0;
 }
 
 /*
@@ -174,137 +192,65 @@ int whatLetter(char c) {
  * once again, you can only use recursion, no loops
  */
 int strCompare2(char* str1, char* str2) {
-    if (*str1 * *str2 == 0) {
-        *str1 == *str2 ? return true : return false;
+    char char1 = whatLetter(*str1);
+    char char2 = whatLetter(*str2);
+    if (!*str1 || !*str2) {
+        if (*str2 && (char2 != -1)) { return -1; }
+        if (*str1 && (char1 != -1)) { return 1; }
+        return 0;
     }
-    if (*str1 == *str2 &&
-        whatLetter(*str1) != -1) {
-            return false
-        }
-        return strCompare(str1+1, str2+1);
+    if (char1 == -1 || char2 == -1) {
+        if (char1 == -1) { ++str1; }
+        if (char2 == -1) { ++str2; }
+        return strCompare2(str1, str2);
     }
-    return false;
+    if (char1 < char2) {
+        return -1;
+    }
+    if (char1 > char2) {
+        return 1;
+    }
+    return strCompare2(++str1, ++str2);
 }
 
-/*
- * the two dimensional array maze[MATRIX_SIZE][MATRIX_SIZE] contains a maze
- * Each square is (initially) either a 1 or a zero.  Each 1 represents a wall
- * (you cannot walk through walls, so you can't go into any square where the 
- * value is 1).  Each 0 represents an open space.  
- *
- * Write an recursive solution to find your way out of the maze
- * Your starting point is at row and col. (params to this function)
- * Your exit point is somewhere in the last row (which is: MATRIX_SIZE - 1)
- *
- * There is a relatively easy recursive solution to this problem, the trick is
- * to think of the solution in the following terms:
- *   "Does the shortest path to the exit go through the current square 
- *   (indicated by row and col)?"
- * With that hint, the base case should be pretty obvious.  In fact,
- * I'd suggest you consider the possibility that the base case is "Yup, the 
- * shortest path to the exit goes through here, I know 'cause I'm already at 
- * the exit!"
- *
- * If you're not at the base case, then ask the following question about each
- * of your four adjacent cells:
- *     Does the shortest path to the exit go through there?
- * If the answer were yes, then leaving the maze would be easy, just go to that
- * square.
- * So, the recursive maze solving problem has a boolean (true/false) return
- * value.  It returns true if the shortest path to the exit goes through
- * the square (row, col) and returns false otherwise.  When row and col
- * are the starting point, the answer will always be true (this function
- * always return true to main).  However, some of the recursive calls will
- * return false.
- *
- * There is one tricky part to this decomposition.  You need to make certain 
- * that the problem is getting smaller.  The "bigness" or "smallness" of this
- * problem is the number of squares that have not yet been tested.  Each time
- * you test an adjacent square (making a recursive call to decide if the
- * shortest path to the exit goes through that square), you'll be reducing 
- * the number of squares that have not yet been tested.  Eventually, you must
- * have tested all the squares and so you'll have to have found a way to the 
- * exit.
- *
- * The easy way to deal with the tricky part is to leave "bread crumbs" behind.
- * Before you recursively check any (or all) of your neighbors to see if you 
- * can find the exit from there -- drop a bread crumb in your current square.
- * Then, don't ever check to see if you can find the exit using a square
- * with a breadcrumb in it (you've already checked that square, or at least are
- * in the process of checking it).
- *
- * If you're trying to see if you can find an exit from some square, and all 
- * the adjacent squares are either walls, or have bread crumbs in them, then
- * you already know the answer -- "The shortest path to the exit does NOT go 
- * through this square".  Pick up your bread crumb and return false.
- * 
- * You can set the value of the current square to "2" to indicate that a bread
- * crumb has been dropped.  Set the square back to 0 when you want to pick 
- * the bread crumb back up.
- * be sure not to change any of the squares that are 1 (the walls).
- *
- * for partial credit, you can leave all your bread crumbs on the floor.
- * for full credit, you must pick up all the bread crumbs EXCEPT those
- * along the shortest path to the exit.
+/* return false if there is a wall in the square for row and col
+ * return true if it's not a wall.
  */
-
-void printMaze(int maze[MATRIX_SIZE][MATRIX_SIZE]) {
-    for (int x = 0; x < MATRIX_SIZE; ++x) {
-        for (int y = 0; y < MATRIX_SIZE; ++y) {
-            putchar('0'+maze[y][x]);
-        }
-    }
+int isOK(int row, int col) {
+    return (row > 0 && row < MATRIX_SIZE
+            && col > 0 && col < MATRIX_SIZE
+            && maze[row][col] == 0);
 }
 
+/* Blah blah blah
+ */
 int solveMazeRec(int row, int col) {
-    //maze[row][col] = 2;   
-    //if (row == MATRIX_SIZE) {
-    //    return 1;
-    //}
-    ///* ⇧ */
-    //if (isOK(row-1, col)) {
-    //    if 
-    //    return solveMazeRec(row-1, col);
-    //}
-    ///* ⇨ */
-    //if (isOK(row, col+1)) {
-    //    return solveMazeRec(row, col+1);
-    //}
-    ///* ⇩ */
-    //if (isOK(row+1, col)) {
-    //    return solveMazeRec(row+1, col);
-    //}
-    ///* ⇦ */
-    //if (isOK(row, col-1)) {
-    //    return solveMazeRec(row, col-1);
-    //}
+    maze[row][col] = 2;   
+    if (row+1 == MATRIX_SIZE) {
+        return 1;
+    }
+    /* ⇧  */
+    if (isOK(row-1, col) && solveMazeRec(row-1, col)) {
+        return 1;
+    }
+    /* ⇨  */
+    if (isOK(row, col+1) && solveMazeRec(row, col+1)) {
+        return 1;
+    }
+    /* ⇩  */
+    if (isOK(row+1, col) && solveMazeRec(row+1, col)) {
+        return 1;
+    }
+    /* ⇦  */
+    if (isOK(row, col-1) && solveMazeRec(row, col-1)) {
+        return 1;
+    }
+    /* ☹  */
+    maze[row][col] = 0;
     return 0;
 }
 
-/**********************
- * adjacentCell and isOK are functions provided to help you write
- * solveMazeIt()
- */
-
-/*
- * OK, we're currently at maze[row][col] and we're considering moving
- * in direction dir.  
- * Set trow and tcol (local variables inside the calling function)
- * to the row and column that we would move to IF we moved in
- * that direction
- *
- * For example, there are two good ways to use this function.
- * 1. to actually move one step in a direction use:
- *       adjacentCell(row, col, dir, &row, &col);
- *    That will set row and col to new values.  The new values will
- *    be one square away from the old values.
- *
- * 2. to set trow and tcol to a square that is adjacent to row and col use:
- *       adjacentCell(row, col, dir, &trow, &tcol);
- *    That will not change row and col, but will change trow and tcol.
- *    This is useful if you aren't sure if you can actually move in this 
- *    direction yet (e.g., maze[trow][tcol] may be a wall!).  So, you set
- *    trow and tcol, and then check to see if it's OK to move there
+/* Herp Blah Derp
  */
 void adjacentCell(int row, int col, int dir, int* trow, int* tcol) {
     *trow = row;
@@ -323,17 +269,6 @@ void adjacentCell(int row, int col, int dir, int* trow, int* tcol) {
             *tcol = *tcol - 1;
             break;
     }
-}
-
-
-/* 
- * return false if there is a wall in the square for row and col
- * return true if it's not a wall.
- */
-int isOK(int row, int col) {
-    return (row > 0 && row < MATRIX_SIZE
-            && col > 0 && col < MATRIX_SIZE
-            && maze[row][col] != 1);
 }
 
 /*
@@ -418,74 +353,42 @@ struct Martian {
     int dodeks;
 };
 
+int valueOf(Martian combo) {
+    return combo.pennies + combo.nicks*VALUE_NICKLE + combo.dodeks*VALUE_DODEK;
+}
 /*
- * using recursion, with no loops or globals, write a function that calculates the optimal
- * (fewest total coins) change for a given amount of money. Return a Martian struct that indicates
- * this optimal collection of coins.
- */
+   * returns the number of coins represented in a Martian
+   */
+int coins(Martian combo) {
+    return combo.pennies + combo.nicks + combo.dodeks;
+}
+
+/*
+   * calculates the optimal change for a given amount of money
+   */
 Martian change(int cents) {
-    //Martian status;
-    ///* Trivial cases */
-    //if (cents%2 == 1 && cents < 15) {
-    //    return combination(-1, 0, 0);
-    //}
-    //if (cents >= dodeks) {
-    //    max_dodeks = cents/DODEK_VALUE;
-    //    /* If the value is expressible in purely dodeks */
-    //    if (cents == max_dodeks*DODEK_VALUE) {
-    //        return combination(0, 0, max_dodeks);
-    //    } else {
-    //        status = change(cents-max_dodeks*DODEK_VALUE);
-    //        if (status.pennies != -1) {
-    //            return status;
-    //        } else {
-    //            return change(cents-(max_dodeks-1)*DODEK_VALUE);
-    //        }
-    //    }
-    //} else if (cents >= nicks) {
-    //    max_nicks = cents/NICK_VALUE;
-    //    if (cents == max_nicks*NICK_VALUE) {
-    //        return combination(0, 0, max_nicks);
-    //    } else {
-    //        status = change(cents-max_nicks*NICK_VALUE);
-    //        if (status.pennies != -1) {
-    //            return status;
-    //        } else {
-    //            return change(cents-(max_nicks-1)*NICK_VALUE);
-    //        }
-    //    }
-    //} else if (cents >= dodeks) {
-    //    max_dodeks = cents/DODEK_VALUE;
-    //    if (cents == max_dodeks*DODEK_VALUE) {
-    //        return combination(0, 0, max_dodeks);
-    //    } else {
-    //        status = change(cents-max_dodeks*DODEK_VALUE);
-    //        if (status.pennies != -1) {
-    //            return status;
-    //        } else {
-    //            return change(cents-(max_dodeks-1)*DODEK_VALUE);
-    //        }
-    //    }
-    //}
-    //        
+    Martian c, nick_combo, t_combo;
+    /* Special case */
+    if (cents == 0) { return { 0,0,0 }; }
+    /* sort of special case */
+    if (cents > 21) {
+        t_combo = change(cents - 12);
+        ++t_combo.dodeks;
+        return t_combo;
+    }
+    c = change(cents - 1);
+    ++c.pennies;
+    if (cents >= 5) {
+        nick_combo = change(cents - 5);
+        ++nick_combo.nicks;
+    } else { return c; }
+    if (cents >= 12) {
+        t_combo = change(cents - 12);
+        ++t_combo.dodeks;
+    } else { return nick_combo; }
+    if (valueOf(nick_combo) == cents && valueOf(t_combo) == cents) {
+        if (coins(nick_combo) < coins(t_combo)) {
+            return nick_combo;
+        } else { return t_combo; }
+    } else { return t_combo; }
 }
-
-/* 
- * without using recursion, solve the Martian change problem described above.
- * NOTE: I don't have a solution to this, and I consider it to be "challenging"
- * (i.e, an exercise in futility). But, if you like banging your head against
- * the wall for zero credit, this is the problem for you!
- */
-Martian changeIt(int cents) {
-    int dodeks = cents<0xa?0:(cents-10)/12+
-                           ((0x001110011100>>((11-((cents-10)%12))*4))&0xf);
-    int nicks = cents<0xa?(cents>4):
-                          (0x220003311144>>((11-((cents-10)%12))*4))&0xf;
-    int pennies = cents<0xa?cents%5:
-                            (0x010120101201>>((11-((cents-10)%12))*4))&0xf;
-    return {pennies, nicks, dodeks};
-    //int dodeks = cents<0xa?0:(0x36c>>11-(cents-10)%12)%2+(cents-10)/12;
-    //int pennies = cents<0xa?cents%5:(0x5ad>>(11-((cents-10)%12)))%2+
-    //              (0x84>>(11-((cents-10)%12)))%2;
-}
-
