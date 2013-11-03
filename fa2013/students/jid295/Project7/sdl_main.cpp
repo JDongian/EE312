3eab3381e96442df10354417f963c9a895f04847
Joshua <jdong42@gmail.com>
2013-11-03 01:48:21 -0500

    Added frame capping and statistics printing.

--- fa2013/students/jid295/Project7/sdl_main.cpp
+++ fa2013/students/jid295/Project7/sdl_main.cpp
@@ -1,5 +1,7 @@
 #include "Bugs.h"
 #include "SDL/SDL.h"
+#include "SDL/SDL_image.h"
+#include "SDL/SDL_ttf.h"
 #include <time.h>
 #include <string>
 
@@ -13,6 +15,8 @@
 
 int world[WORLD_SIZE][WORLD_SIZE]; 
 std::vector<Bug> bug_list;
+
+TTF_Font *font = NULL;
 
 void put_pixel(SDL_Surface *surface, int x, int y, int pixel) {
     int bpp = surface->format->BytesPerPixel;
@@ -41,20 +45,58 @@ void put_pixel(SDL_Surface *surface, int x, int y, int pixel) {
     }
 }
 
+void apply_surface(int x, int y,
+                   SDL_Surface* source, SDL_Surface* destination) {
+    SDL_Rect offset;
+    offset.x = x;
+    offset.y = y;
+    SDL_BlitSurface(source, NULL, destination, &offset);
+}
+
+void render_text(int x, int y, SDL_Surface *surface,
+                 TTF_Font* font, char buff[], SDL_Color c) {
+    int lineskip = TTF_FontLineSkip(font);
+    int line = 0;
+    int i = 0;
+    char* begin = buff;
+    while (buff[i]) {
+        if (buff[i] == '\n') {
+            buff[i] = '\0';
+            apply_surface(x, y + line++*lineskip,
+                          TTF_RenderText_Blended(font, begin, c),
+                          surface);         
+            while(buff[i] == '\n' ||
+                  buff[i] == '\0') {
+                ++i;
+            }
+            begin = buff+i;
+        }
+        ++i;
+    }
+    apply_surface(x, y + line*lineskip,
+                  TTF_RenderText_Blended(font, begin, c),
+                  surface);
+}
+
 int sdl_init() {
     if (SDL_Init(SDL_INIT_EVERYTHING) == -1) { return false; }
-    if(SDL_EnableUNICODE(1) == -1) { return false; }
     SDL_WM_SetCaption("Bug's Life", NULL);
     return true;
 }
 
-SDL_Surface* reconfig(bool fullscreen, char zoom_level) {
+TTF_Font* ttf_init() {
+    if(TTF_Init() == -1) { return NULL; }  
+    if(SDL_EnableUNICODE(1) == -1) { return NULL; }
+    return TTF_OpenFont("Ubuntu-L.ttf", 12);
+}
+
+SDL_Surface* config(bool fullscreen, char zoom_level) {
     if (fullscreen) {
-        //printf("Reconfiguring screen to fullscreen mode.\n");
+        //printf("configuring screen to fullscreen mode.\n");
         return SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT,
                                 SCREEN_BPP, SDL_FULLSCREEN);
     } else {
-        //printf("Reconfiguring windowed screen to at %d zoom.\n",
+        //printf("configuring windowed screen to at %d zoom.\n",
         //        zoom_level);
         return SDL_SetVideoMode(WORLD_SIZE*zoom_level,
                                 (WORLD_SIZE+42)*zoom_level,
@@ -146,8 +188,15 @@ void render_world(int world[WORLD_SIZE][WORLD_SIZE],
     }
 }
 
+void tick(unsigned int n) {
+    for (int i = n; i > 0; --i) {
+        timeStep();
+    }
+}
+
 int main(int argc, char* args[]) {
     SDL_Surface* screen = NULL;
+    TTF_Font *font = NULL;
     SDL_Event event;
     Uint8* keystates;
     bool quit = false;
@@ -156,26 +205,34 @@ int main(int argc, char* args[]) {
     bool running = false;
     int tick_once = 0;
     int step_size = 1;
-    int step = 0;
     int t_prev = 0;
-    int FPS = 20;
+    bool cap = true;
+    char run_stats[64];
+    char world_stats[256];
+    unsigned int FRAMES_PER_SECOND = 60;
+    double measured_fps = 0;
+
+    /* Seed random to time. */
     srand(time(NULL));
     /* Initialize the game world. */
     createWorld();
     /* Create the default screen. */
-    screen = SDL_SetVideoMode(WORLD_SIZE*zoom_level,
-            (WORLD_SIZE+42)*zoom_level,
-            SCREEN_BPP,
-            SDL_SWSURFACE);
+    screen = config(fullscreen, zoom_level);
     if (!screen) {
         printf("Failed initializing screen. Aborting.\n");
         return 1;
     }
+    /* Initialize the SDL window and title. */
     if (!sdl_init()) {
         printf("Failed initializing SDL. Aborting.\n");
         return 1;
     }
-    /* Render 1 frame for aesthetics. */
+    /* Load the font and SDL_ttf. */
+    if (!(font = ttf_init())) {
+        printf("Failed initializing TTF. Aborting.\n");
+        return 1;
+    }
+    /* Render and display one frame for aesthetics. */
     SDL_FillRect(screen, &screen->clip_rect,
                  SDL_MapRGB(screen->format, 0x77, 0x77, 0x77));
     render_world(world, screen, fullscreen, zoom_level);
@@ -183,51 +240,61 @@ int main(int argc, char* args[]) {
         printf("Failed switching buffers. Aborting.");
         return 1;
     }
+    /* Simulation loop. */
     while (!quit) {
+        /* Event handler */
         keystates = SDL_GetKeyState(NULL);
+        if(keystates[SDLK_LEFT]) {
+            if (step_size > 1) {
+                --step_size;
+            }
+        }
+        if(keystates[SDLK_RIGHT]) {
+            if (step_size < 1024) {
+                ++step_size;
+            }
+        }
         if (SDL_PollEvent(&event)) {
             if (event.type == SDL_KEYDOWN) {
-                /* Hotkey: 'q' for quit. */
-                if(keystates[SDLK_LEFT]) {
-                    if (step_size > 1) {
-                        --step_size;
-                    }
-                }
-                if(keystates[SDLK_RIGHT]) {
-                    if (step_size < 1024) {
-                        ++step_size;
-                    }
-                }
                 switch(event.key.keysym.sym) {
+                    /* Hotkey: '+' for expand window. */
                     case SDLK_EQUALS:
-                        if(keystates[SDLK_LSHIFT] ||
-                           keystates[SDLK_RSHIFT]) {
-                            if (zoom_level < 3) {
-                                ++zoom_level;
-                                if (!fullscreen) {
-                                    screen = reconfig(fullscreen, zoom_level);
+                        /* Only change window size if not fullscreen. */
+                        if (!fullscreen) {
+                            if(keystates[SDLK_LSHIFT] ||
+                               keystates[SDLK_RSHIFT]) {
+                                if (zoom_level < 4) {
+                                    ++zoom_level;
+                                    screen = config(fullscreen,
+                                                      zoom_level);
                                     tick_once = 1;
                                 }
                             }
                         }
                         break;
+                    /* Hotkey: '-' to contract window. */
                     case SDLK_MINUS:
-                        if (zoom_level > 1) {
-                            --zoom_level;
-                            if (!fullscreen) {
-                                screen = reconfig(fullscreen, zoom_level);
+                        /* Only change window size if not fullscreen. */
+                        if (!fullscreen) {
+                            if (zoom_level > 1) {
+                                --zoom_level;
+                                screen = config(fullscreen, zoom_level);
                                 tick_once = 1;
                             }
                         }
                         break;
+                    /* Hotkey: <F12> to toggle fullscreen. */
                     case SDLK_F12:
                         fullscreen = !fullscreen;
-                        screen = reconfig(fullscreen, zoom_level);
+                        screen = config(fullscreen, zoom_level);
                         tick_once = 1;
                         break;
+                    /* Hotkey: <SPACE> to pause/unpause. */
                     case SDLK_SPACE:
                         running = !running;
                         break;
+                    /* Hotkey: <ENTER> to tick once.
+                     * Hotkey: <CTRL>+<ENTER> to tick 1000.*/
                     case SDLK_RETURN:
                         tick_once = 1;
                         if(keystates[SDLK_LSHIFT] ||
@@ -235,10 +302,12 @@ int main(int argc, char* args[]) {
                             tick_once = 1000;
                         }
                         break;
+                    /* Hotkey: 'q' to quit. */
                     case SDLK_q:
                         printf("Quit received.\n");
                         quit = true;
                         break;
+                    /* Hotkey: <ESC> to quit. */
                     case SDLK_ESCAPE:
                         printf("Quit received.\n");
                         quit = true;
@@ -251,35 +320,44 @@ int main(int argc, char* args[]) {
                 quit = true;
             }
         }
+        /* Set time before render to t_prev. */
         t_prev = SDL_GetTicks();
         SDL_FillRect(screen, &screen->clip_rect,
                      SDL_MapRGB(screen->format, 0x77, 0x77, 0x77));
         if (running) {
-            for (step = step_size; step > 0; --step) {
-                timeStep();
-            }
-            render_world(world, screen, fullscreen, zoom_level);
-            if (SDL_Flip(screen) == -1) {
-                printf("Failed switching buffers. Aborting.");
-                return 1;
-            }
+            tick(step_size);
         } else if (tick_once) {
-            while (tick_once > 0) {
-                --tick_once;
-                timeStep();
-            }
+            tick(tick_once);
+            tick_once = 0;
+        }
+        if (running || tick_once) {
             render_world(world, screen, fullscreen, zoom_level);
+            /* Render running statistics at the bottom. */
+            sprintf(run_stats, "FPS: %d, STEP: %d",
+                    (int)measured_fps, step_size);
+            render_text(WORLD_SIZE*zoom_level/2, WORLD_SIZE*zoom_level+3, screen,
+                        font, run_stats, {0x00, 0x00, 0x00});
+            sprintf(world_stats, "Time: %d\n"
+                                 "Pop: %d\n"
+                                 "Average age: %d\n"
+                                 "Average generation: %d",
+                    time_step, (int)bug_list.size(),
+                    average_age, average_generation);
+            render_text(6, WORLD_SIZE*zoom_level+3, screen,
+                        font, world_stats, {0x00, 0x00, 0x00});
             if (SDL_Flip(screen) == -1) {
                 printf("Failed switching buffers. Aborting.");
                 return 1;
             }
         }
-        if(t_prev % 16 == 0) {
-            printf("FPS: %d            \r",
-                   (int)(1000.0/(SDL_GetTicks()-t_prev)));
-            fflush(stdout);
+        /* Frame rate limit */
+        if((cap == true) && (SDL_GetTicks()-t_prev < 1000/FRAMES_PER_SECOND)) {
+            SDL_Delay((1000/FRAMES_PER_SECOND) - (SDL_GetTicks()-t_prev));
         }
+        measured_fps = 1000./(SDL_GetTicks()-t_prev);
     }
+    TTF_CloseFont(font);
+    TTF_Quit();
     SDL_FreeSurface(screen);
     SDL_Quit();
     printf("Simulation ended.\n");
