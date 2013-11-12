0643505dd9dbaad1631c3aa8e45a67a92786b66c
Joshua <jdong42@gmail.com>
2013-11-12 14:02:31 -0600

    fixed indentation for my preferences.

--- fa2013/students/jid295/Project8/Project8.cpp
+++ fa2013/students/jid295/Project8/Project8.cpp
@@ -4,12 +4,10 @@
 FILE* input_stream;
 
 bool readNum(int& x) {
-	return EOF != fscanf(input_stream, "%d", &x);
+    return EOF != fscanf(input_stream, "%d", &x);
 }
 
 int main(void) {
-	input_stream = fopen("input.txt", "r");
+    input_stream = fopen("input.txt", "r");
 
 }
-
-
